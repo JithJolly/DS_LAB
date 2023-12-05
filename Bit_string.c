3d75f7c38b05ae5f49d6f814eb4e4b33808fa9f0
Jith Jolly <145551249+JithJolly@users.noreply.github.com>
2023-12-05 09:36:53 +0530

    Update Bit_string.c

--- Bit_string.c
+++ Bit_string.c
@@ -1,233 +1,210 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-// Define the structure for a node in the linked list
-struct Node {
-    int data;
-    struct Node* next;
-};
+int superSet[20],superSetSize=0,
+setA[20],setASize=0,
+setB[20],setBSize=0,
+bitStringA[20],bitStringB[20],bitStringUnion[20],bitStringIntersection[20],bitStringDifference[20];
+int isBitStringReady=0;
+void getSet(int arr[],int setSize);
+void processChoice(int choice);
+void printAllSets();
+void printSet(int arr[],int size);
+void generateAndPrintBitStrings();
+void generateBitString(int arr[],int size,int bitStringArray[]);
+int search(int arr[],int arrSize,int elem);
+int checkBitStringStatus();
+void setUnion(int arr1[],int arr2[]);
+void setIntersection(int arr1[],int arr2[]);
+void setDifference(int arr1[],int arr2[]);
+void printSetOperationResult(int arr[]);
+void printBitStringAsSet(int arr[]);
+int checkBitStringStatus();
 
-// Function to create a new node
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
-}
+int main(){
+    int choice=0;
+    system("cls");
+    do{
 
-// Function to insert a node at the beginning of the linked list
-void insertAtBeginning(struct Node** head, int data) {
-    struct Node* newNode = createNode(data);
-    newNode->next = *head;
-    *head = newNode;
-    printf("Node inserted at the beginning.\n");
-}
-
-// Function to insert a node at the end of the linked list
-void insertAtEnd(struct Node** head, int data) {
-    struct Node* newNode = createNode(data);
-    struct Node* temp = *head;
-
-    if (*head == NULL) {
-        *head = newNode;
-        printf("Node inserted at the end.\n");
-        return;
-    }
-
-    while (temp->next != NULL) {
-        temp = temp->next;
-    }
-    temp->next = newNode;
-    printf("Node inserted at the end.\n");
-}
-
-// Function to insert a node at a specific position in the linked list
-void insertAtPosition(struct Node** head, int data, int position) {
-    if (position < 1) {
-        printf("Invalid position. Position should be greater than 0.\n");
-        return;
-    }
-
-    if (position == 1) {
-        insertAtBeginning(head, data);
-        return;
-    }
-
-    struct Node* newNode = createNode(data);
-    struct Node* temp = *head;
-
-    for (int i = 1; i < position - 1 && temp != NULL; i++) {
-        temp = temp->next;
-    }
-
-    if (temp == NULL) {
-        printf("Position is out of range.\n");
-        return;
-    }
-
-    newNode->next = temp->next;
-    temp->next = newNode;
-    printf("Node inserted at position %d.\n", position);
-}
-
-// Function to delete a node at the beginning of the linked list
-void deleteAtBeginning(struct Node** head) {
-    if (*head == NULL) {
-        printf("List is empty. Nothing to delete.\n");
-        return;
-    }
-
-    struct Node* temp = *head;
-    *head = (*head)->next;
-    free(temp);
-    printf("Node deleted from the beginning.\n");
-}
-
-// Function to delete a node at the end of the linked list
-void deleteAtEnd(struct Node** head) {
-    if (*head == NULL) {
-        printf("List is empty. Nothing to delete.\n");
-        return;
-    }
-
-    if ((*head)->next == NULL) {
-        free(*head);
-        *head = NULL;
-        printf("Node deleted from the end.\n");
-        return;
-    }
-
-    struct Node* temp = *head;
-    while (temp->next->next != NULL) {
-        temp = temp->next;
-    }
-
-    free(temp->next);
-    temp->next = NULL;
-    printf("Node deleted from the end.\n");
-}
-
-// Function to delete a node at a specific position in the linked list
-void deleteAtPosition(struct Node** head, int position) {
-    if (*head == NULL) {
-        printf("List is empty. Nothing to delete.\n");
-        return;
-    }
-
-    if (position < 1) {
-        printf("Invalid position. Position should be greater than 0.\n");
-        return;
-    }
-
-    struct Node* temp = *head;
-    if (position == 1) {
-        *head = temp->next;
-        free(temp);
-        printf("Node deleted from position %d.\n", position);
-        return;
-    }
-
-    for (int i = 1; i < position - 1 && temp != NULL; i++) {
-        temp = temp->next;
-    }
-
-    if (temp == NULL || temp->next == NULL) {
-        printf("Position is out of range.\n");
-        return;
-    }
-
-    struct Node* nodeToDelete = temp->next;
-    temp->next = temp->next->next;
-    free(nodeToDelete);
-    printf("Node deleted from position %d.\n", position);
-}
-
-// Function to search for an element in the linked list
-void searchElement(struct Node* head, int key) {
-    int position = 1;
-    while (head != NULL) {
-        if (head->data == key) {
-            printf("Element %d found at position %d.\n", key, position);
-            return;
-        }
-        head = head->next;
-        position++;
-    }
-    printf("Element %d not found in the list.\n", key);
-}
-
-// Function to display the linked list
-void displayList(struct Node* head) {
-    printf("Linked List: ");
-    while (head != NULL) {
-        printf("%d -> ", head->data);
-        head = head->next;
-    }
-    printf("NULL\n");
-}
-
-int main() {
-    struct Node* head = NULL;
-    int choice, data, position;
-
-    do {
-        printf("\nLinked List Operations\n");
-        printf("1. Insert at Beginning\n");
-        printf("2. Insert at End\n");
-        printf("3. Insert at Position\n");
-        printf("4. Delete at Beginning\n");
-        printf("5. Delete at End\n");
-        printf("6. Delete at Position\n");
-        printf("7. Search Element\n");
-        printf("8. Display Linked List\n");
-        printf("9. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                printf("Enter data to insert at beginning: ");
-                scanf("%d", &data);
-                insertAtBeginning(&head, data);
-                break;
-            case 2:
-                printf("Enter data to insert at end: ");
-                scanf("%d", &data);
-                insertAtEnd(&head, data);
-                break;
-            case 3:
-                printf("Enter position to insert: ");
-                scanf("%d", &position);
-                printf("Enter data to insert at position %d: ", position);
-                scanf("%d", &data);
-                insertAtPosition(&head, data, position);
-                break;
-            case 4:
-                deleteAtBeginning(&head);
-                break;
-            case 5:
-                deleteAtEnd(&head);
-                break;
-            case 6:
-                printf("Enter position to delete: ");
-                scanf("%d", &position);
-                deleteAtPosition(&head, position);
-                break;
-            case 7:
-                printf("Enter element to search: ");
-                scanf("%d", &data);
-                searchElement(head, data);
-                break;
-            case 8:
-                displayList(head);
-                break;
-            case 9:
-                printf("Exiting the program...\n");
-                break;
-            default:
-                printf("Invalid choice. Please enter a valid choice.\n");
-        }
-    } while (choice != 9);
-
+	printf("1.Enter Universal Set\n");
+	printf("2.Enter Set A\n");
+	printf("3.Enter Set B\n");
+	printf("4.Generate Bit Strings\n");
+	printf("5.Union\n");
+	printf("6.Intersection\n");
+	printf("7.Difference\n");
+	printf("8.Print Sets\n");
+	printf("9.Exit\n");
+	printf("Enter your Choice:");
+	scanf("%d",&choice);
+	processChoice(choice);
+    }while(choice!=9);
     return 0;
+}
+
+void processChoice(int choice){
+    switch(choice){
+	case 1:
+	    printf("Enter Super Set Size:");
+	    scanf("%d",&superSetSize);
+	    getSet(superSet,superSetSize);
+	    break;
+	case 2:
+	    printf("Enter Set A Size:");
+	    scanf("%d",&setASize);
+	    getSet(setA,setASize);
+	    break;
+	case 3:
+	    printf("Enter Set B Size:");
+	    scanf("%d",&setBSize);
+	    getSet(setB,setBSize);
+	     break;
+	case 4:
+	    printf("Generating bit strings\n");
+	    generateAndPrintBitStrings();
+	     break;
+	case 5:
+	    printf("Set union\n");
+	    if(checkBitStringStatus()==1)
+	    {
+		setUnion(bitStringA,bitStringB);
+		printSetOperationResult(bitStringUnion);
+	    }
+	     break;
+	case 6:
+	    printf("Set Intersection\n");
+	    if(checkBitStringStatus()==1)
+	    {
+		setIntersection(bitStringA,bitStringB);
+		printSetOperationResult(bitStringIntersection);
+	    }
+	     break;
+	case 7:
+	    printf("Set Difference\n");
+	    if(checkBitStringStatus()==1)
+	    {
+		setDifference(bitStringA,bitStringB);
+		printSetOperationResult(bitStringDifference);
+	    }
+	     break;
+	case 8:
+	    printAllSets();
+	     break;
+    }
+}
+
+void printSetOperationResult(int arr[]){
+    printf("\nUnion Operation (bit string):");
+    printSet(arr,superSetSize);
+    printf("\nOperation Result:");
+    printBitStringAsSet(arr);
+}
+
+void printBitStringAsSet(int arr[]){
+    int isFirstOutputDoneFlag=0;
+    int i=0;
+    printf("{");
+    for( i=0;i<superSetSize;i++){
+	if(arr[i]==1){
+	    if(i!=0 && isFirstOutputDoneFlag==1){
+		printf(",");
+	    }
+	    printf("%d",superSet[i]);
+	    isFirstOutputDoneFlag=1;
+	}
+    }
+    printf("}");
+}
+void setUnion(int arr1[],int arr2[]){
+	int i;
+    for(i=0;i<superSetSize;i++){
+	bitStringUnion[i]=arr1[i]|arr2[i];
+    }
+}
+void setIntersection(int arr1[],int arr2[]){
+	int i;
+    for(i=0;i<superSetSize;i++){
+	bitStringIntersection[i]=arr1[i]&arr2[i];
+    }
+}
+void setDifference(int arr1[],int arr2[]){
+	int i;
+    for(i=0;i<superSetSize;i++){
+	printf("%d",!arr2[i]) ;
+	bitStringDifference[i]=arr1[i]&(!arr2[i]);
+    }
+}
+int checkBitStringStatus(){
+    if(isBitStringReady==0){
+	printf("\n Generate Bit String first!");
+	return 0;
+    }
+    return 1;
+}
+void getSet(int arr[],int setSize){
+	int i;
+    printf("\nEnter set\n");
+    for(i=0;i<setSize;i++){
+	scanf("%d",&arr[i]);
+    }
+}
+
+void printAllSets(){
+
+    printf("\nSuper Set:");
+    printSet(superSet,superSetSize);
+    printf("Set A:");
+    printSet(setA,setASize);
+    printf("Set B:");
+    printSet(setB,setBSize);
+}
+
+void printSet(int arr[],int size){
+	int i;
+    printf("{");
+    for(i=0;i<size;i++){
+	printf("%d",arr[i]);
+	if(i!=size-1){
+	    printf(",");
+	}
+    }
+    printf("}\n");
+}
+
+void generateAndPrintBitStrings(){
+	int i;
+    for(i=0;i<superSetSize;i++){
+	bitStringA[i]=0;
+	bitStringB[i]=0;
+	bitStringUnion[i]=0;
+	bitStringIntersection[i]=0;
+	bitStringDifference[i]=0;
+    }
+    generateBitString(setA,setASize,bitStringA);
+
+    generateBitString(setB,setBSize,bitStringB);
+
+    printf("\nSet A Bit String representation : ");
+    printSet(bitStringA,superSetSize);
+    printf("\nSet B Bit String representation : ");
+    printSet(bitStringB,superSetSize);
+    isBitStringReady=1;
+}
+
+void generateBitString(int arr[],int size,int bitStringArray[]){
+	int i;
+    for(i=0;i<size;i++){
+	int pos=search(superSet,superSetSize,arr[i]);
+	if(pos>=0){
+	    bitStringArray[pos]=1;
+	}
+    }
+}
+int search(int arr[],int arrSize,int elem){
+	int i;
+    for(i=0;i<arrSize;i++){
+	if(arr[i]==elem)
+	    return i;
+    }
+    return -1;
 }
